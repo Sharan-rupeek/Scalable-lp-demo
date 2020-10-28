4963aca2070834bca00ab8fdf398b6571048dfd0
Sharan.ram <sharan.ram@Rupeeks-MacBook-Pro.local>
2020-10-28 23:01:26 +0530

    Create LeadFlow component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import React, {useState} from 'react'
-import data from './data'
+import React from 'react'
 
-import Number from './Components/Number'
-import GoldWeight from './Components/GoldWeight'
-import OTP from './Components/OTP'
-import Pincode from './Components/Pincode'
+import LeadFlow from './Containers/LeadFlow'
 
-function App() {
-  const [currentStep, setCurrentStep] = useState(1)
-
-  const renderPopup = () => {
-    const currentPopup = data.leadFlow.find(popupItem => popupItem.step === currentStep)
-
-    const getPopUpComponent = () => {
-      switch(currentPopup.lead) {
-        case 'Number': return Number
-        case 'GoldWeight': return GoldWeight
-        case 'OTP': return OTP
-        case 'Pincode': return Pincode
-        default: return null
-      }
-    }
-
-    const PopupComponent =  getPopUpComponent()
-
-    return <PopupComponent handleSubmit={() => setCurrentStep(currentStep + 1)} />
-  }
-
-  return (
-    <div className="App">
-      {renderPopup()}
-    </div>
-  );
+const App = () => {
+    return <LeadFlow />
 }
 
-export default App;
+export default App
---
+++ src/Containers/LeadFlow.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import data from '../data'
+
+import Number from '../Components/Number'
+import GoldWeight from '../Components/GoldWeight'
+import OTP from '../Components/OTP'
+import Pincode from '../Components/Pincode'
+
+function LeadFlow() {
+  const [currentStep, setCurrentStep] = useState(1)
+
+  const renderPopup = () => {
+    const currentPopup = data.leadFlow.find(popupItem => popupItem.step === currentStep)
+
+    const getPopUpComponent = () => {
+      switch(currentPopup.lead) {
+        case 'Number': return Number
+        case 'GoldWeight': return GoldWeight
+        case 'OTP': return OTP
+        case 'Pincode': return Pincode
+        default: return null
+      }
+    }
+
+    const PopupComponent =  getPopUpComponent()
+
+    return <PopupComponent handleSubmit={() => setCurrentStep(currentStep + 1)} />
+  }
+
+  return (
+    <div className="App">
+      {renderPopup()}
+    </div>
+  );
+}
+
+export default LeadFlow;
