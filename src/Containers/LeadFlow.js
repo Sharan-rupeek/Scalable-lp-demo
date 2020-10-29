811e1c02e6b3683e24dec1a10fcebf2280bfdbae
Sharan.ram <sharan.ram@Rupeeks-MacBook-Pro.local>
2020-10-29 11:46:36 +0530

    Implement Done component

--- src/Containers/LeadFlow.js
+++ src/Containers/LeadFlow.js
@@ -6,24 +6,27 @@ import GoldWeight from '../Components/GoldWeight'
 import OTP from '../Components/OTP'
 import Pincode from '../Components/Pincode'
 
+const DoneComponent = () => {
+  return (
+    <div>Done</div>
+  )
+}
+
 function LeadFlow() {
   const [currentStep, setCurrentStep] = useState(1)
-
   const renderPopup = () => {
+    if (currentStep > data.leadFlow.length) return <DoneComponent />
     const currentPopup = data.leadFlow.find(popupItem => popupItem.step === currentStep)
-
     const getPopUpComponent = () => {
       switch(currentPopup.lead) {
-        case 'Number': return Number
+        case 'Number': return Number    
         case 'GoldWeight': return GoldWeight
         case 'OTP': return OTP
         case 'Pincode': return Pincode
         default: return null
       }
     }
-
     const PopupComponent =  getPopUpComponent()
-
     return <PopupComponent handleSubmit={() => setCurrentStep(currentStep + 1)} />
   }
 
